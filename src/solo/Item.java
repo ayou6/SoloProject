172fd1640f69a3a8e08e8cbc2fa38ff6c9c1ce30
ayou6 <ayoxb.abbas@gmail.com>
2023-03-06 00:24:31 -0800

    Invoiceupdate

--- src/solo/Item.java
+++ src/solo/Item.java
@@ -1,12 +1,31 @@
 package solo;
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Item {
 	
+    Scanner scn = new Scanner(System.in);
+
 	String itemName;
-    int Id;
-    double Price;
+    int id;
+    double price;
     int quantity;
 
-     ArrayList itemList = new ArrayList();
+     static ArrayList<Item> itemList = new ArrayList();
+     public void addItem(Scanner scn) {
+         System.out.print("Enter item name: ");
+         String itemName = scn.nextLine();
+         System.out.print("Enter item ID: ");
+         int id = scn.nextInt();
+         System.out.print("Enter item price: ");
+         double price = scn.nextDouble();
+         System.out.print("Enter item quantity: ");
+         int quantity = scn.nextInt();
+         
+         Item newItem = new Item();
+         itemList.add(newItem);
+         
+         System.out.println("Item added successfully.");
+     }
 
 }
+
