99ca8730f452db5795c2abd1ddf6106ac8ad7a1b
ayou6 <ayoxb.abbas@gmail.com>
2023-03-05 23:13:58 -0800

    Invoiceupdate

--- src/solo/Main.java
+++ src/solo/Main.java
@@ -11,6 +11,14 @@ public class Main {
 	static Invoice inv = new Invoice();
 	
 	public static void main(String[] args) {
+		int cntStat1 = 0;
+		int cntStat2 = 0;
+		int cntStat3 = 0;
+		int cntStat4 = 0;
+		int cntStat5 = 0;
+		int cntStat6 = 0;
+		int cntStat7 = 0;
+		int cntStat8 = 0;
 		boolean loop = false;
 		while (loop == false) {
 		Mainmenu.Menu();
@@ -18,6 +26,8 @@ public class Main {
 			
 	        switch (choose) {
 	        case "1":
+				cntStat1++;
+
 	        	ShopSet.ShopSet();
                 break;
                 
@@ -26,26 +36,77 @@ public class Main {
             	break;
             	
             case "3":
-            	
-            	
+        		cntStat3++;
+        		/*
+        		if (Main.inv.invoiceList.isEmpty()) {
+					System.out.println("Add Invoice!");
+				} else {
+					System.out.printf("%20s %20s %20s %20s %20s\n", "INVO NO", "Tel", "Fax",
+							"Email", "website");
+					System.out.println(
+							"__________________________________________________________________________________________________________________");
+					for (int i = 0; i < Main.inv.invoiceList.size(); i++) {
+						for (int j = 0; j < Main.inv.invoiceList.size(); j++) {
+							System.out.printf("%20s %20s %20s %20s %20s\n", Main.inv.invoNO,
+									mySchool.studentList.get(i).stuName, mySchool.studentList.get(i).mailList,
+									mySchool.subjectList.get(j),
+									mySchool.studentList.get(i).studentSubjectList.get(j).studentMark.mark);
+						}
+					}
+				}
+
+			}
+            	*/
             	break;
             	
             case "4":
-            	
+        		cntStat4++;
+        
             	break;
             	
             case "5":
-            	
+        		cntStat5++;
             	break;
             case "6":
-            	
-            	break;
+            	/*{
+            	cntStat6++;
+				int x = 0;
+				boolean loop1 = true;
+				while (loop1) {
+					System.out.println("Enter Invoice NO: ");
+					int searchInvo = scn.nextInt();
+					for (int i = 0; i < Main.inv.invoiceList.size(); i++) {
+						if (Main.inv.invoNO.equal(searchInvo))
+							System.out.println("School Name: " + mySchool.name);
+							System.out.println("Student Name:" + mySchool.studentList.get(i).stuName);
+							for (int j = 0; j < mySchool.subjectList.size(); j++) {
+								System.out.println(mySchool.subjectList.get(j) + " mark is: "
+										+ mySchool.studentList.get(i).studentSubjectList.get(j).studentMark.mark);
+							}
+							x = x + 1;
+						}
+					}
+					if (x == 0) {
+						System.out.println("NOT FOUND!!!");
+			}
+				}*/
+				break;
+
             	
             case "7":
-            	
+        		cntStat7++;
+     		   System.out.println("[1] Shop Settings" + cntStat1);
+    		   System.out.println("[2] Manage Shop Items" + cntStat2);
+    		   System.out.println("[3] Create New Invoice" + cntStat3);
+    		   System.out.println("[4] Report: Statistics " + cntStat4);
+    		   System.out.println("[5] Report: All Invoices" + cntStat5);
+    		   System.out.println("[6] Search" + cntStat6);
+    		   System.out.println("[7] Program Statistics" + cntStat7);
+    		   System.out.println("[8] Exit" + cntStat8);;
             	break;
             	
             case "8":
+            	cntStat8++;
             	System.out.println("Good Bye!!!");
                 loop = true;
 
