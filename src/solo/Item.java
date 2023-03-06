94c0096806b5cc2622dd7d64dc9f1b3bf90c5ccc
ayou6 <ayoxb.abbas@gmail.com>
2023-03-06 04:54:30 -0800

    Invoiceupdate

--- src/solo/Item.java
+++ src/solo/Item.java
@@ -1,69 +1,98 @@
 package solo;
+
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Item {
-	
+
     static Scanner scn = new Scanner(System.in);
 
-	String itemName;
+    String itemName;
     int id;
     double price;
     int quantity;
 
-     static ArrayList<Item> itemList = new ArrayList();
-     public void addItem() {
-     	Boolean Load = true;
-        while (Load) {
-         System.out.print("Enter item name: ");
-         String itemName = scn.nextLine();
-         System.out.print("Enter item ID: ");
-         int id = scn.nextInt();
-         System.out.print("Enter item price: ");
-         double price = scn.nextDouble();
-         System.out.print("Enter item quantity: ");
-         int quantity = scn.nextInt();
-         
-         Item newItem = new Item();
-         itemList.add(newItem);
-         
-         System.out.println("Item added successfully.");
-         System.out.println("Do you want to add more? y/n");
-         String more = scn.next();
-         if (more.equalsIgnoreCase("y")) {
-         	 Load = true;
-         } else {
-             Load = false;
-         }break;
+    static ArrayList<Item> itemList = new ArrayList<Item>();
+
+    public void addItem() {
+        boolean load = true;
+        while (load) {
+            System.out.print("Enter item name: ");
+            String itemName = scn.next();
+            System.out.print("Enter item ID: ");
+            int id = scn.nextInt();
+            scn.nextLine();
+            System.out.print("Enter item price: ");
+            double price = scn.nextDouble();
+            scn.nextLine(); 
+            System.out.print("Enter item quantity: ");
+            int quantity = scn.nextInt();
+            scn.nextLine();
+
+            Item newItem = new Item();
+            newItem.itemName = itemName;
+            newItem.id = id;
+            newItem.price = price;
+            newItem.quantity = quantity;
+            itemList.add(newItem);
+
+            System.out.println("Item added successfully.");
+            System.out.println("Do you want to add more? y/n");
+            String more = scn.next();
+            if (more.equalsIgnoreCase("y")) {
+                load = true;
+            } else {
+                load = false;
+                break;
+            }
+        }
+    }
+
+    public static void deleteItem() {
+        System.out.print("Enter item ID to delete: ");
+        int id = scn.nextInt();
+
+        boolean found = false;
+        for (Item item : itemList) {
+            if (item.id == id) {
+                itemList.remove(item);
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Item deleted successfully.");
+        } else {
+            System.out.println("Item not found.");
+        }
+    }
+
+    public static void changeItemPrice() {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter item ID: ");
+        int itemId = scn.nextInt();
+        System.out.print("Enter new item price: ");
+        double newPrice = scn.nextDouble();
+
+        for (Item item : itemList) {
+            if (item.id == itemId) {
+                item.price = newPrice;
+                System.out.println("Item price changed successfully!");
+                return;
+            }
+        }
+        System.out.println("Item not found.");
+    }
+
+    public static void reportAllItems() {
+        System.out.println("-----------");
+        System.out.println("Report All Items");
+        System.out.println("-----------");
+
+        if (itemList.isEmpty()) {
+            System.out.println("No items to report.");
+            return;
+        }
 }
-
-     }
-     
-     public static void deleteItem() {
-         System.out.print("Enter item ID to delete: ");
-         int id = scn.nextInt();
-         
-         
-         // search for item with matching ID and remove from itemList
-         boolean found = false;
-         for (Item item : itemList) {
-             if (item.getId() == id) {
-                 itemList.remove(item);
-                 found = true;
-                 break;
-             }
-         }
-         
-         if (found) {
-             System.out.println("Item deleted successfully.");
-         } else {
-             System.out.println("Item not found.");
-         }
-     }
-
-	private int getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
 }
-
--- src/solo/Menu.java
+++ src/solo/Menu.java
@@ -1,64 +1,62 @@
 package solo;
+
 public class Menu {
-   public void Menu() {
-	   System.out.println(" -----------");
-	   System.out.println("| Main Menu |");
-	   System.out.println(" -----------");
-	   System.out.println("[1] Shop Settings");
-	   System.out.println("[2] Manage Shop Items");
-	   System.out.println("[3] Create New Invoice");
-	   System.out.println("[4] Report: Statistics ");
-	   System.out.println("[5] Report: All Invoices");
-	   System.out.println("[6] Search");
-	   System.out.println("[7] Program Statistics");
-	   System.out.println("[8] Exit");
+	public void Menu() {
+		System.out.println(" -----------");
+		System.out.println("| Main Menu |");
+		System.out.println(" -----------");
+		System.out.println("[1] Shop Settings");
+		System.out.println("[2] Manage Shop Items");
+		System.out.println("[3] Create New Invoice");
+		System.out.println("[4] Report: Statistics ");
+		System.out.println("[5] Report: All Invoices");
+		System.out.println("[6] Search");
+		System.out.println("[7] Program Statistics");
+		System.out.println("[8] Exit");
 
-   }
-   public void Menu2() {
-	   System.out.println(" -----------");
-	   System.out.println("| Manage Shop items |");
-	   System.out.println(" -----------");
-	   System.out.println("[1] add");
-	   System.out.println("[2] delete");
-	   System.out.println("[3] change price");
-	   System.out.println("[4] Report");
-       System.out.println("[5] Go Back");
-       String choose = Main.scn.next();
-       switch (choose) {
-           case "1":
-           	Boolean Load = true;
-               while (Load) {
-            	   Main.Itm.addItem();
-            	   
-            	   
-            	   
+	}
+
+	public void Menu2() {
+		Boolean Load = true;
+		while (Load) {
+			System.out.println(" -----------");
+			System.out.println("| Manage Shop items |");
+			System.out.println(" -----------");
+			System.out.println("[1] add");
+			System.out.println("[2] delete");
+			System.out.println("[3] change price");
+			System.out.println("[4] Report");
+			System.out.println("[5] Go Back");
+			String choose = Main.scn.next();
+			switch (choose) {
+			case "1":
+
+				Main.Itm.addItem();
+
+				break;
+
+			case "2":
+				Main.Itm.deleteItem();
+
+				break;
+
+			case "3":
+				Main.Itm.changeItemPrice();
+				break;
+
+			case "4":
+				Main.Itm.reportAllItems();
+				break;
+
+			case "5":
+				System.out.println("Good Bye!!!");
+				break;
+
+			default:
+				System.out.println("Invalid option. Please choose again.");
+				break;
+			}
+		}
+	}
+
 }
-               break;
-               
-           case "2":
-        	   Main.Itm.deleteItem();
-        	   
-
-           	break;
-           	
-           case "3":
-
-           	break;
-           	
-           	
-           case "4":
-              
-               break;
-               
-           case "5":
-        	   break;
-        	   
-           default:
-               System.out.println("Invalid option. Please choose again.");
-               break;
-       }
-      
-   }
-   
-}
-   
